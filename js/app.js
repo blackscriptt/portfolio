468a469f97cc6f8c29d665d4789cc83a1dd4f40e
black script <111915039+blackscriptt@users.noreply.github.com>
2023-04-30 01:14:32 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-
-    // loading - [START]
-    $('#loading .loading__first').animate({ opacity: '1' })
-      .delay(7000)
-      .animate({ opacity: '0' },
-      function () {
-        $('#loading').animate({ opacity: '1' })
-          .delay(1500)
-          .animate({ opacity: '0' }, function() { $(this).css({ display: 'none' }) });
-      });
-    // loading - [END^]
-
     $('.aside__close').on('click', function() {
         $('aside').animate({
             left: '-200%'
